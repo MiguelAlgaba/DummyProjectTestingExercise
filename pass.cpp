93f7ae0d70adcef9a8eb485d26aa84571f24d11d
Miguel Algaba <miguel.algaba.borrego@gmail.com>
2012-10-24 12:34:13 +0200

    First commit to the master branch.

---
+++ pass.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    if(1==1)
+    {
+        std::cerr<<"SUCCESS"<<std::endl;
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        std::cerr<<"FAILURE"<<std::endl;
+        return EXIT_FAILURE;
+    }
+}
